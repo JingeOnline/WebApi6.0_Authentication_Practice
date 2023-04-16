d13a3e91ae3a953893af3258f2a4715933bdc775
Jinge Zhang <jinge_windows@hotmail.com>
2023-04-17 01:07:39 +1000

    添加了代码，来测试读写Cookie

--- WebApi_Authentication_Demo/WebApi_BasicAuthentication/Controllers/UserController.cs
+++ WebApi_Authentication_Demo/WebApi_BasicAuthentication/Controllers/UserController.cs
@@ -20,8 +20,26 @@ namespace WebApi_BasicAuthentication.Controllers
         [HttpGet]
         public IActionResult Get()
         {
+            //该代码是用来测试在Response中添加cookie -------------------------------------
+            CookieOptions cookieOptions = new CookieOptions()
+            {
+                HttpOnly = true,
+                Expires = DateTime.Now.AddMinutes(1)
+            };
+            Response.Cookies.Append("Test", $"This is a test cooky, will expired at {cookieOptions.Expires}.", cookieOptions);
+            //-------------------------------------------------------------------------------
+
             List<string> userNames = _userRepository.GetUserNames();
             return Ok(userNames);
+        }
+
+        //这个Action是用来测试读取客户端的cookie
+        [HttpGet]
+        [Route("cookie")]
+        public IActionResult GetCookie()
+        {
+            string test = Request.Cookies["Test"];
+            return Ok(test);
         }
     }
 }
