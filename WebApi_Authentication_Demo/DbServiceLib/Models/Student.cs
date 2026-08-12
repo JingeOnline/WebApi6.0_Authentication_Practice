35b72f77fa95b8356300603b35a6dc51cadacb12
Jinge <jinge_windows@hotmail.com>
2026-08-13 01:54:05 +1000

    添加了两条注释

--- WebApi_Authentication_Demo/DbServiceLib/Models/Student.cs
+++ WebApi_Authentication_Demo/DbServiceLib/Models/Student.cs
@@ -13,6 +13,8 @@ namespace DbServiceLib.Models
         public string Name { get; set; }
         public bool Gender { get; set; }
         public int Age { get; set; }
+        //这里使用virtual是为了启用延迟加载（Lazy Loading）功能，这样在访问Subjects属性时，相关的Subject数据会在需要时才从数据库中加载，
+        //而不是在查询Student时立即加载所有相关的Subject数据。这有助于提高性能，尤其是在处理大量数据时。
         public virtual List<Subject> Subjects { get; set; }
     }
 }
--- WebApi_Authentication_Demo/DbServiceLib/Models/Subject.cs
+++ WebApi_Authentication_Demo/DbServiceLib/Models/Subject.cs
@@ -12,6 +12,7 @@ namespace DbServiceLib.Models
         public int PkId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        //这里使用virtual是为了启用延迟加载（Lazy Loading）功能，这样在访问Students属性时，相关的Student数据会在需要时才从数据库中加载，
         public virtual List<Student> Students { get; set; }
     }
 }
